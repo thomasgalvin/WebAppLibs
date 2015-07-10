092185fe28e1f4616bda1030a84484ea6fdcb213
Thomas Galvin <tgalvin@caci.com>
2015-07-10 15:32:28 -0400

    Added angular-ui-router

--- src/main/java/weblib/WebLibsDeploy.java
+++ src/main/java/weblib/WebLibsDeploy.java
@@ -13,7 +13,7 @@ public class WebLibsDeploy
     public static void registerAssetBundles( Bootstrap bootstrap ) {
         bootstrap.addBundle( new AssetsBundle( "/ace/1.1.9", CONTEXT_ROOT_ACE, "", "weblib-ace" ) );
         bootstrap.addBundle( new AssetsBundle( "/angular/1.4.2", CONTEXT_ROOT_ANGULAR, "", "weblib-angular" ) );
-        bootstrap.addBundle( new AssetsBundle( "/bootstrap/3.5.5", CONTEXT_ROOT_BOOTSTRAP, "", "weblib-bootstrap" ) );
+        bootstrap.addBundle( new AssetsBundle( "/bootstrap/3.3.5", CONTEXT_ROOT_BOOTSTRAP, "", "weblib-bootstrap" ) );
         bootstrap.addBundle( new AssetsBundle( "/ui-bootstrap/0.13.0", CONTEXT_ROOT_UI_BOOTSTRAP, "", "weblib-bootstrap-ui" ) );
     }
 }
