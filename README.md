c5ecb1b2bd3f860a9a348d492c50b53064285e8b
Thomas Galvin <tgalvin@caci.com>
2015-07-10 13:39:27 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WebAppLibs
+
+A collection of HTML/CSS/Javascript libraries, packaged up for deployment in 
+DropWizard.
---
+++ src/main/java/weblib/DropWizardConfig.java
@@ -0,0 +1,19 @@
+package weblib;
+
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+
+public class DropWizardConfig
+{
+    public static final String CONTEXT_ROOT_ACE= "ace";
+    public static final String CONTEXT_ROOT_ANGULAR = "ng";
+    public static final String CONTEXT_ROOT_BOOTSTRAP = "bootstrap";
+    public static final String CONTEXT_ROOT_UI_BOOTSTRAP = "ui-bootstrap";
+    
+    public static void registerAssetBundles( Bootstrap bootstrap ) {
+        bootstrap.addBundle( new AssetsBundle( "ace/1.1.9", CONTEXT_ROOT_ACE, "", "weblib-ace" ) );
+        bootstrap.addBundle( new AssetsBundle( "angular/1.4.2", CONTEXT_ROOT_ANGULAR, "", "weblib-angular" ) );
+        bootstrap.addBundle( new AssetsBundle( "bootstrap/3.5.5", CONTEXT_ROOT_BOOTSTRAP, "", "weblib-bootstrap" ) );
+        bootstrap.addBundle( new AssetsBundle( "ui-bootstrap/0.13.0", CONTEXT_ROOT_UI_BOOTSTRAP, "", "weblib-bootstrap-ui" ) );
+    }
+}
