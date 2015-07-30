2d433d69208e116b3d2d7bf9a631190bbf3e8aad
Thomas Galvin <tgalvin@caci.com>
2015-07-30 19:16:28 -0400

    Added jQuery

--- src/main/java/weblib/WebLibs.java
+++ src/main/java/weblib/WebLibs.java
@@ -9,11 +9,13 @@ public class WebLibs
     public static final String CONTEXT_ROOT_ANGULAR = "/ng";
     public static final String CONTEXT_ROOT_BOOTSTRAP = "/bootstrap";
     public static final String CONTEXT_ROOT_UI_BOOTSTRAP = "/ui-bootstrap";
+    public static final String CONTEXT_ROOT_JQUERY = "/jquery";
     
     public static void registerAssetBundles( Bootstrap bootstrap ) {
         bootstrap.addBundle( new AssetsBundle( "/ace/1.1.9", CONTEXT_ROOT_ACE, "", "weblib-ace" ) );
         bootstrap.addBundle( new AssetsBundle( "/angular/1.4.2", CONTEXT_ROOT_ANGULAR, "", "weblib-angular" ) );
         bootstrap.addBundle( new AssetsBundle( "/bootstrap/3.3.5", CONTEXT_ROOT_BOOTSTRAP, "", "weblib-bootstrap" ) );
         bootstrap.addBundle( new AssetsBundle( "/ui-bootstrap/0.13.0", CONTEXT_ROOT_UI_BOOTSTRAP, "", "weblib-bootstrap-ui" ) );
+        bootstrap.addBundle( new AssetsBundle( "/jQuery/2.1.3", CONTEXT_ROOT_JQUERY, "", "weblib-jquery" ) );
     }
 }
