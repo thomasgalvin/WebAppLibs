b4f87706a26275cdb0e138e3eb740e9b7e34162e
Thomas Galvin <tgalvin@caci.com>
2015-08-04 13:34:45 -0400

    Added angular-ui-tree to WebAppLibs

--- src/main/java/weblib/WebLibs.java
+++ src/main/java/weblib/WebLibs.java
@@ -7,6 +7,7 @@ public class WebLibs
 {
     public static final String CONTEXT_ROOT_ACE= "/ace";
     public static final String CONTEXT_ROOT_ANGULAR = "/ng";
+    public static final String CONTEXT_ROOT_ANGULAR_UI_TREE = "/ng-ui-tree";
     public static final String CONTEXT_ROOT_BOOTSTRAP = "/bootstrap";
     public static final String CONTEXT_ROOT_UI_BOOTSTRAP = "/ui-bootstrap";
     public static final String CONTEXT_ROOT_JQUERY = "/jquery";
@@ -14,6 +15,7 @@ public class WebLibs
     public static void registerAssetBundles( Bootstrap bootstrap ) {
         bootstrap.addBundle( new AssetsBundle( "/ace/1.1.9", CONTEXT_ROOT_ACE, "", "weblib-ace" ) );
         bootstrap.addBundle( new AssetsBundle( "/angular/1.4.2", CONTEXT_ROOT_ANGULAR, "", "weblib-angular" ) );
+        bootstrap.addBundle( new AssetsBundle( "/angular-ui-tree/2.0", CONTEXT_ROOT_ANGULAR_UI_TREE, "", "weblib-angular-ui-tree" ) );
         bootstrap.addBundle( new AssetsBundle( "/bootstrap/3.3.5", CONTEXT_ROOT_BOOTSTRAP, "", "weblib-bootstrap" ) );
         bootstrap.addBundle( new AssetsBundle( "/ui-bootstrap/0.13.0", CONTEXT_ROOT_UI_BOOTSTRAP, "", "weblib-bootstrap-ui" ) );
         bootstrap.addBundle( new AssetsBundle( "/jQuery/2.1.3", CONTEXT_ROOT_JQUERY, "", "weblib-jquery" ) );
---
+++ src/main/js/angular-ui-tree/2.0/examples/js/app.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+
+  angular.module('demoApp', ['ui.tree', 'ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when('/basic-example', {
+          controller: 'BasicExampleCtrl',
+          templateUrl: 'views/basic-example.html'
+        })
+        .when('/connected-trees', {
+          controller: 'ConnectedTreesCtrl',
+          templateUrl: 'views/connected-trees.html'
+        })
+        .when('/filter-nodes', {
+          controller: 'FilterNodesCtrl',
+          templateUrl: 'views/filter-nodes.html'
+        })
+        .otherwise({
+          redirectTo: '/basic-example'
+        });
+    }]);
+})();
