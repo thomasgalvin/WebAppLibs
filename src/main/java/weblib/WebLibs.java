a222a93b2683b01d79bd1530f7a7856533561722
Thomas Galvin <tgalvin@caci.com>
2015-08-12 13:18:55 -0400

    Added starry sky bakcground image in /img-common, along with license info

--- src/main/java/weblib/WebLibs.java
+++ src/main/java/weblib/WebLibs.java
@@ -11,6 +11,7 @@ public class WebLibs
     public static final String CONTEXT_ROOT_BOOTSTRAP = "/bootstrap";
     public static final String CONTEXT_ROOT_UI_BOOTSTRAP = "/ui-bootstrap";
     public static final String CONTEXT_ROOT_JQUERY = "/jquery";
+    public static final String CONTEXT_ROOT_IMG_COMMON = "/img-common";
     
     public static void registerAssetBundles( Bootstrap bootstrap ) {
         bootstrap.addBundle( new AssetsBundle( "/ace/1.1.9", CONTEXT_ROOT_ACE, "", "weblib-ace" ) );
@@ -19,5 +20,6 @@ public class WebLibs
         bootstrap.addBundle( new AssetsBundle( "/bootstrap/3.3.5", CONTEXT_ROOT_BOOTSTRAP, "", "weblib-bootstrap" ) );
         bootstrap.addBundle( new AssetsBundle( "/ui-bootstrap/0.13.0", CONTEXT_ROOT_UI_BOOTSTRAP, "", "weblib-bootstrap-ui" ) );
         bootstrap.addBundle( new AssetsBundle( "/jQuery/2.1.3", CONTEXT_ROOT_JQUERY, "", "weblib-jquery" ) );
+        bootstrap.addBundle( new AssetsBundle( "/img-common", CONTEXT_ROOT_IMG_COMMON, "", "weblib-img-common" ) );
     }
 }
