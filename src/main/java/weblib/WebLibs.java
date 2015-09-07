a63d5cad537a7d0bac417fe24c39574f3522aebf
Thomas Galvin <tgalvin@caci.com>
2015-09-07 15:44:49 -0400

    Added Skeleton framework

--- src/main/java/weblib/WebLibs.java
+++ src/main/java/weblib/WebLibs.java
@@ -12,6 +12,7 @@ public class WebLibs
     public static final String CONTEXT_ROOT_UI_BOOTSTRAP = "/ui-bootstrap";
     public static final String CONTEXT_ROOT_JQUERY = "/jquery";
     public static final String CONTEXT_ROOT_IMG_COMMON = "/img-common";
+    public static final String CONTEXT_ROOT_SKELETON = "/skeleton";
     
     public static void registerAssetBundles( Bootstrap bootstrap ) {
         bootstrap.addBundle( new AssetsBundle( "/ace/1.1.9", CONTEXT_ROOT_ACE, "", "weblib-ace" ) );
@@ -21,5 +22,6 @@ public class WebLibs
         bootstrap.addBundle( new AssetsBundle( "/ui-bootstrap/0.13.0", CONTEXT_ROOT_UI_BOOTSTRAP, "", "weblib-bootstrap-ui" ) );
         bootstrap.addBundle( new AssetsBundle( "/jQuery/2.1.3", CONTEXT_ROOT_JQUERY, "", "weblib-jquery" ) );
         bootstrap.addBundle( new AssetsBundle( "/img-common", CONTEXT_ROOT_IMG_COMMON, "", "weblib-img-common" ) );
+        bootstrap.addBundle( new AssetsBundle( "/skeleton/2.0.4", CONTEXT_ROOT_SKELETON, "", "weblib-skeleton" ) );
     }
 }
